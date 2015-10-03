c1c660fbc767e7bf5424445945c7fbfe765b877a
Andrew Nguyen <andrew.nguyen42@gmail.com>
2015-10-03 10:27:20 -0500

    man, cleaning up data sucks

--- rushingscrape.js
+++ rushingscrape.js
@@ -21,20 +21,24 @@ var casper = require('casper').create({
 
 
 //this is the first year we have NFL data from
-var firstYear = 1932
-var lastYear = 2014
+var firstYear = 1932;
+var lastYear = 2014;
 
 
 
-var baseurl = "http://www.pro-football-reference.com/"
+var baseurl = "http://www.pro-football-reference.com/";
 
-var urls = []
+var urls = [];
 
+//ugh I don't want to parse the urls for the year
+var linkYearMap = {}
 for (i = firstYear; i <= lastYear; i++) {
-    urls.push(baseurl + "years/" + i + "/rushing.htm")
+    urls.push(baseurl + "years/" + i + "/rushing.htm");
+    linkYearMap[urls[urls.length - 1]] = i;
+
 }
 
-
+var csvData = "Rk,,Tm,Age,Pos,G,GS,Att,Yds,TD,Lng,Y/A,Y/G,A/G,Rec,Yds,Y/R,TD,Lng,R/G,Y/G,YScm,RRTD,Fmb \n";
 
 casper.start(baseurl, function() {
     this.echo(this.getTitle());
@@ -42,14 +46,33 @@ casper.start(baseurl, function() {
 
     self.thenOpen(link, function() {
         this.echo(this.getTitle());
-	this.clickLabel('CSV', 'span');
-	this.waitForSelector('#csv_rushing_and_receiving',function(){
-		this.echo('got that csv data');
-	
-	})
+
+        this.clickLabel('CSV', 'span');
+        this.waitForSelector('#csv_rushing_and_receiving', function() {
+            var tokens = this.fetchText('#csv_rushing_and_receiving').split('\n');
+
+            tokens = tokens.filter(function(element) {
+                return !(element.length === 0 || !element.trim()) && element.substring(0, 4) != ',,,,' && element.substring(0, 4) != 'Rk,,'
+            })
+
+
+            this.echo(tokens.length)
+            tokens.forEach(function(entry, index) {
+                casper.echo(entry);
+                tokens[index] = tokens[index] += "," + linkYearMap[link]
+                casper.echo(tokens[index])
+
+            });
+        })
     });
 
 })
 
+casper.then(function() {
+
+    this.echo(csvData);
+
+
+})
 
 casper.run();
